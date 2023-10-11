abb05da4f0141b4b8d4692d86b30273fabc6e256
Andrey <test@test>
2023-10-11 23:56:24 +0300

    developer bootcamp 2 ok

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// using static ArrayCreator;
+// using static ArrayMath;
+using System.Diagnostics;
+
+
+int[] array = 10_000.Create()
+                .Fill(1, 10);
+
+// array.ConvertToStringAndPrintToTerminal();
+
+Stopwatch sw = new Stopwatch();
+int m = 1_000;
+int max;
+
+sw.Start();
+max = array.BadGetSum(m);
+sw.Stop();
+Console.WriteLine($"BadGetSum: {max} => {sw.ElapsedMilliseconds}");
+
+sw.Reset();
+
+sw.Start();
+max = array.GoodGetSum(m);
+sw.Stop();
+Console.WriteLine($"GoodGetSum: {max} => {sw.ElapsedMilliseconds}");
